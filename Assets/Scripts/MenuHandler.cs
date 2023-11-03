8cca81613f4d36c71a8d4257dfa3b4ef43ba0ec0
freerangerstudios <freeranger05@gmail.com>
2023-11-03 18:30:53 +0100

    small ui improvements

--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 public class MenuHandler : MonoBehaviour
 {
     public TMP_InputField text;
+    public TextMeshProUGUI splashText;
 
     public static string username = "balls420";
     
@@ -15,13 +16,17 @@ public class MenuHandler : MonoBehaviour
     public Button guideButton;
     public Button quitButton;
 
+    public List<string> splashTexts = new();
+    
     private void Start()
     {
         playButton.onClick.AddListener(OnPlayClick);
         guideButton.onClick.AddListener(OnGuideClick);
         quitButton.onClick.AddListener(OnQuitClick);
-    }
 
+        splashText.text = splashTexts[Random.Range(0, splashTexts.Count)];
+    }
+    
     private void OnPlayClick()
     {
         if (text.text != "")
